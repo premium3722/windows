408d8c7c231cd22d4cf8cebc961a19b461de02e8
premium3722 <levin.vonkaenel@hotmail.com>
2024-06-17 11:38:27 +0200

    Create WCD_setup.ps1

---
+++ WCD_setup.ps1
@@ -0,0 +1,12 @@
+$provisioning = "$($env:ProgramData)\provisioning"
+
+ni $provisioning -ItemType Directory -Force | Out-Null
+
+gci -Path . | ?{$_.name -ne "setup.ps1"} | %{
+    cp $_.FullName (Join-Path -Path $provisioning -ChildPath $_.name) -Force
+}
+
+New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name "execute_provisioning" -Value ("cmd /c powershell.exe -ExecutionPolicy Bypass -File `"{0}\provisioning.ps1`"" -f $provisioning)
+Set-ExecutionPolicy unrestricted
+
+
