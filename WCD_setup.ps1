2ec5ef9bc4a71bc424c22ebebc64caeb606372f6
Premium <levin.vonkaenel@hotmail.com>
2024-08-11 17:11:03 +0200

    Update WCD_setup.ps1

--- WCD_setup.ps1
+++ WCD_setup.ps1
@@ -6,7 +6,7 @@ gci -Path . | ?{$_.name -ne "setup.ps1"} | %{
     cp $_.FullName (Join-Path -Path $provisioning -ChildPath $_.name) -Force
 }
 
-New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name "execute_provisioning" -Value ("cmd /c powershell.exe -ExecutionPolicy Bypass -File `"{0}\provisioning.ps1`"" -f $provisioning)
+New-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" -Name "execute_provisioning" -Value ("cmd /c powershell.exe -ExecutionPolicy Bypass -File `"{0}\WCD_provisioning_start.ps1`"" -f $provisioning)
 Set-ExecutionPolicy unrestricted
 
 
